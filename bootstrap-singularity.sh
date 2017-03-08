1d46dad963f6a2b36093983377ccd9349a964245
Rober Morales-Chaparro <rober.morales@gmail.com>
2017-03-08 11:36:08 +0100

    reduce log size, rm image without confirmation

--- bootstrap-singularity.sh
+++ bootstrap-singularity.sh
@@ -5,4 +5,4 @@ image=`basename $PWD`-`date +%Y%m%d-%H%M%S -u`.img
 sudo singularity create -s 4000 $image
 sudo singularity bootstrap $image singularity.def
 lz4 $image $image.lz4
-rm $image
+rm -f $image
