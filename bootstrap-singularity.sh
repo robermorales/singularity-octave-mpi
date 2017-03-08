8c1c0417a13aa41fdb4cb9a304b62c769a79b979
Rober Morales-Chaparro <rober.morales@gmail.com>
2017-03-08 11:10:43 +0100

    octave, openmpi, example added

--- bootstrap-singularity.sh
+++ bootstrap-singularity.sh
@@ -3,5 +3,6 @@
 image=`basename $PWD`-`date +%Y%m%d-%H%M%S -u`.img
 
 sudo singularity create -s 4000 $image
-sudo singularity bootstrap $image singularity.def > singularity-bootstrap.log 2>&1
-
+sudo singularity bootstrap $image singularity.def
+lz4 $image $image.lz4
+rm $image
